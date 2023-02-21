7ce04e4ee210448643450b6e67076aa3dd8267bd
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-02-21 16:51:18 +0200

    add file start

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import css from './Filter.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectFilter } from './../../redux/filter/filterSelector';
+import { filterContactsAction } from 'redux/filter/filter-slice';
+
+const Filter = () => {
+  const filterValue = useSelector(selectFilter);
+  const dispatch = useDispatch();
+  return (
+    <div className={css.filter}>
+      <label className={css.filter__label}>
+        Find contacts by name
+        <input
+          className={css.filter__input}
+          name="filter"
+          type="text"
+          value={filterValue}
+          onChange={e => dispatch(filterContactsAction(e.target.value))}
+          placeholder="Filter contacts"
+        />
+      </label>
+    </div>
+  );
+};
+export default Filter;
